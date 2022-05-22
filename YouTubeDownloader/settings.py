c6f78bbdd57d425631b6529a60c60254401aa343
amirkey_ <galaxyman0110@gmail.com>
2022-05-22 22:15:34 +0430

    third commit

--- YouTubeDownloader/settings.py
+++ YouTubeDownloader/settings.py
@@ -127,3 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
