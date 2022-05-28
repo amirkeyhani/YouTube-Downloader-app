e62b27664cbac3a762410ba11407396e54fbb987
amirkey_ <galaxyman0110@gmail.com>
2022-05-29 03:56:29 +0430

    edit appname

--- YouTubeDownloader/settings.py
+++ YouTubeDownloader/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(bz2dajy4-46fhbu_#=sot$6k(=kwfp!3+wl3@jt9e4^b&-+e2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['u-uploader.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['u-downloader.herokuapp.com', 'localhost']
 
 
 # Application definition
