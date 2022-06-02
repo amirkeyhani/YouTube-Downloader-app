eb2108774506ef8416ced04e8716824df6da903b
amirkey_ <galaxyman0110@gmail.com>
2022-06-02 22:19:48 +0430

    youtube-downloader

--- youtubedownloaderapp/urls.py
+++ youtubedownloaderapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     # path('', views.home, name='home'),
     # path('download/', views.download_video, name='download'),
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
+    path('', views.home.as_view(), name='home'),
 ]
--- youtubedownloaderapp/views.py
+++ youtubedownloaderapp/views.py
@@ -23,18 +23,55 @@
 #             return redirect('home')
 #     return redirect('home')
 
-from django.shortcuts import render
-from django.contrib import messages
-from pytube import *
+# from django.shortcuts import render
+# from django.contrib import messages
+# from pytube import *
 # Create your views here.
 
 
-def index(request):
-    if request.method == 'POST':
-        url = request.POST['url']
-        video = YouTube(url)
-        stream = video.streams.get_highest_resolution()
-        stream.download()
-        messages.info(request, 'Video Downloaded..')
-        return render(request, 'home.html')
-    return render(request, 'home.html')
+# def index(request):
+#     if request.method == 'POST':
+#         url = request.POST['url']
+#         video = YouTube(url)
+#         stream = video.streams.get_highest_resolution()
+#         stream.download()
+#         messages.info(request, 'Video Downloaded..')
+#         return render(request, 'home.html')
+#     return render(request, 'home.html')
+
+
+# from django.views.generic import View
+# from pytube import YouTube
+# from django.shortcuts import render, redirect
+
+# class home(View):
+#     def __init__(self, url=None):
+#         self.url = url
+        
+#     def get(self, request):
+#             return render(request, 'home.html')
+        
+#     def post(self, request):
+#         if request.POST.get('fetch-vid'):
+#             self.url = request.POST.get('given_url')
+#             video = YouTube(self.url)
+#             vidTitle, vidThumbnail = video.title, video.thumbnail_url
+#             qual, stream = [], []
+#             for vid in video.streams.filter(progressive=True):
+#                 qual.append(vid.resolution)
+#                 stream.append(vid)
+#             context = {'vidTitle': vidTitle, 'vidThumbnail': vidThumbnail, 
+#                        'qual': qual, 'stream': stream, 
+#                        'url': self.url}
+#             return render(request, 'home.html', context)
+        
+#         elif request.POST.get('download-vid'):
+#             self.url = request.POST.get('given_url')
+#             video = YouTube(self.url)
+#             stream = [x for x in video.streams.filter(progressive=True)]
+#             video_qual = video.streams[int(request.POST.get('download-vid')) - 1]
+#             video_qual.download(output_path='../../Downloads')
+#             return redirect('home')
+        
+#         return render(request, 'home.html')
+
